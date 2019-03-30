a961ec85bdd04ca683b212b10dff36bda889acc0
GuiHaack <guihaack51@gmail.com>
2019-03-29 21:44:43 -0300

    erro input radio form classification release

--- src/app/classifications/classifications.component.ts
+++ src/app/classifications/classifications.component.ts
@@ -1,4 +1,8 @@
+import { Classifications } from './../models/classifications';
+import { first } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ClassificationsService } from './classifications.service';
 
 @Component({
   selector: 'app-classifications',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassificationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private classificationService: ClassificationsService) { }
+
+  classification: Classifications[];
+  class: Classifications;
+  displayClassification: boolean = false;
 
   ngOnInit() {
+    this.getClassification();
   }
 
+  getClassification(){
+    this.classificationService.getClassifications().pipe(first())
+    .subscribe(classifications =>{ this.classification = [... classifications.body.obj] });
+  }
+
+  onDisplayClassification(){
+    this.displayClassification = !this.displayClassification;
+  }
+
+  select(C){
+    this.class = Object.assign({},C);
+    console.log(C);
+    console.log(this.class);
+  }
+
+  deleteClassification(){
+    this.classificationService.delete(this.class.id_classification)
+      .subscribe(
+        resp => {
+          this.class = null;
+          this.getClassification();
+        }
+      ),
+       (
+         error => console.log(error)
+      )
+  }
+
+  onSubmit(p){
+    console.log(p.value);
+    
+    let classific = new FormData();
+    
+    classific.append('name_classification', p.value.name_classification);
+    classific.append('classifation_type', p.value.classifation_type);
+
+    console.log(classific);
+
+    this.classificationService.postClassifications(classific).subscribe((response) => {
+      p.reset();
+      this.getClassification();
+      this.displayClassification = !this.displayClassification;
+      console.log(response);
+    }, error => console.log(error))
+  }
 }
