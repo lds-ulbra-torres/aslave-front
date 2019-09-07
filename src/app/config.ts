f5732b372a8af77f5764b125f61e4a45dbf5e208
JozielOliveira <programdor.joziel@gmail.com>
2019-09-07 14:22:05 -0300

    add new environments

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,11 @@
 //PROD
 // export const baseUrl =  "http://api-aslave-org-br.umbler.net/";
 
-//TESTE
-export const baseUrl =  "http://api-teste-aslave-org-br.umbler.net/";
+//TEST
+// export const baseUrl =  "http://api-teste-aslave-org-br.umbler.net/";
 
 // DEV
-// export const baseUrl =  "http://api-dev-aslave-org-br.umbler.net/";
+export const baseUrl =  "http://api-dev-aslave-org-br.umbler.net/";
+
+// LOCAL
+// export const baseUrl =  "http://localhost:3000/";
